8673a70f8f48028854fadc0742c4ac09a2c06299
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-07-25 16:24:47 +0300

    undefinde

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,20 @@
-export function fetchBreeds(){
-    const KEY = 'live_rEv9nGLilDCvc0lglvJrH2rw3bUAK6aaFuCccjTCugKD7vuBlRJW6SgB1L7JOtF8';
+const KEY = 'live_rEv9nGLilDCvc0lglvJrH2rw3bUAK6aaFuCccjTCugKD7vuBlRJW6SgB1L7JOtF8';
 const END_POINT = '/breeds';
-const BASE_URL = 'https://api.thecatapi.com/v1'
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const SEARCH_URL = 'https://api.thecatapi.com/v1/images/search'
+
+export function fetchBreeds(){
+    
     return fetch(`${BASE_URL}${END_POINT}?api_key=${KEY}`)
     .then(response => {
         return response.json()
+    })
+}
+
+
+export function fetchCatByBreed(breedId){
+    return fetch(`${SEARCH_URL}?breed_ids=${breedId}&api_key=${KEY}`)
+    .then(resp => {
+        return resp.json();
     })
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {fetchBreeds} from './cat-api'
-
+import {fetchCatByBreed} from './cat-api'
 
 
 const refs = {
@@ -16,18 +16,30 @@ const refs = {
 refs.select.addEventListener('change', selectBreeds)
 
 function selectBreeds(evt) {
-return evt.target.value
+const getBreedId = evt.target.value;
+getId(getBreedId);
 }
 
-// function findCat(){
-//     fetchBreeds().then(data => {
-//         const id = data.map(addMarkUpId).join('')
-//         refs.select.insertAdjacentHTML('afterbegin',id)
+
+function getId(id){
+    fetchCatByBreed(id).then(data => {
+const img = data.map(createMarkUpImg).join('');
+refs.info.insertAdjacentHTML('afterbegin',img);
+const breedsInfo = data.map(elem => elem.breeds)
+
+// const dataCat = breedsInfo.flatMap(createMarkUpAboutCat).join('');
+        // refs.info.insertAdjacentHTML('afterbegin',breedsInfo);
+console.log(typeof(breedsInfo))
+
         
-//     }).catch(err => {
-//         console.log(err)
-//     })
-// }
+
+console.log(data)
+})
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 function getBreeds () {
 
     fetchBreeds().then(data => {
@@ -38,6 +50,35 @@ function getBreeds () {
         console.log(err)
     })
 }
+
+
+function addMarkUpId ({id, name}) {
+return `<option value="${id}">${name}</option>`
+}
+
+function createMarkUpImg({url}){
+return `<img src="${url}" alt="cat" width="640" class="cat-info_img">`
+}
+
+function createMarkUpAboutCat({description, name, temperament}){
+    return `<h2 class="cat-info_name">${name}</h2>
+    <p class="cat-info_text">${description}</p>
+    <p>Temperament:${temperament}</p>`
+}
+
+getBreeds()
+
+
+// function findCat(){
+//     fetchBreeds().then(data => {
+//         const id = data.map(addMarkUpId).join('')
+//         refs.select.insertAdjacentHTML('afterbegin',id)
+        
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// }
+// ======================
 // const makeGalleryImagesMarkup = ({url, alt}) => {
 //     return `<li class="gallery_link"><img src=${url} alt=${alt} width = 300 class="gallery_img"></img></li>`;
 //     }
@@ -45,13 +86,3 @@ function getBreeds () {
 //     const galleryImg = images.map(makeGalleryImagesMarkup).join('')
     
 //     galleryList.insertAdjacentHTML("afterbegin",galleryImg);
-
-function addMarkUpId ({id, name}) {
-return `<option value="${id}">${name}</option>`
-}
-
-function createMarkUpAboutCat({image, description, temperament}){
-    return 
-}
-
-getBreeds()
